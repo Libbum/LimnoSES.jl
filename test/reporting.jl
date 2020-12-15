8748b7bf9b1b07c175b4c3bdd15ec0db9beec22f
Tim DuBois <tim@neophilus.net>
2020-12-15 10:50:31 +0100

    Whats still useful in lake and reporting

---
+++ test/reporting.jl
@@ -0,0 +1,22 @@
+@testset "Reporting" begin
+    model = initialise(lake_setup = lake_initial_state(Clear, Scheffer))
+
+    @test nutrients(model) == model.lake.p.nutrients
+    @test vegetation(model.lake) == 50.0
+    @test vegetation([model.lake.u[1]], model.lake.p) == 50.0
+
+    model = initialise(
+        experiment = Experiment(nutrient_series = Dynamic()),
+        lake_setup = lake_initial_state(Clear, Martin),
+        municipalities = Dict(
+            "main" => (
+                Governance(
+                    houseowner_type = Enforced(),
+                    interventions = planner(plan(WastewaterTreatment)),
+                ),
+                100,
+            ),
+        ),
+    )
+    @test upgrade_efficiency(model) == 0
+end
